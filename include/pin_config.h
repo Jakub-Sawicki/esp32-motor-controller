b7ab4fc653f4c8c99b03ab4ff887c1ac81ce9876
Kuba <kuba_sawicki@onet.pl>
2024-03-29 23:34:52 +0100

    added PID controller

--- include/pin_config.h
+++ include/pin_config.h
@@ -1,14 +1,11 @@
 #ifndef PIN_CONFIG_H
 #define PIN_CONFIG_H
 
-#define PWM1 16
-#define PWM2 17
-#define PWM3 18
-#define PWM4 19
+int PWM[4] = {16, 17, 18, 19};
 
-#define A1_PHASE 34
-#define B1_PHASE 35
-#define A2_PHASE 36
-#define B2_PHASE 37
+int A_PHASE[2] = {34, 36};
+int B_PHASE[2] = {35, 37};
+
+#define LED_PIN 16
 
 #endif // PIN_CONFIG_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,36 @@
 #include "pin_config.h"
+#include "PID.h"
+#include "PID.cpp"
 
 #include <Arduino.h>
 
+
+PID pid(1, 1, 1, 1);
+
 void setup() {
   Serial.begin(115200);
+  while(!Serial) {
+    ; // waiting for serial port to connect
+  }
+
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  Serial.println("Hello World!");
-  delay(1000);
+  //Serial.println("Hello BeagleBone!");
+
+  if(Serial.available() > 0) {
+    char message = Serial.read();
+    Serial.print(message);
+
+    if(message = 'h') {
+      digitalWrite(LED_PIN, LOW);
+    }
+    else if(message = 'l') {
+      digitalWrite(LED_PIN, HIGH);
+    }
+  }
+  else {
+    ;
+  }
 }
